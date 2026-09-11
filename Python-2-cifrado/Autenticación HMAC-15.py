84280906a73ade55f0e18083ebb7e17cdb0fa64e
Python-practicas-3-- <114303948+SandraMorales09@users.noreply.github.com>
2026-09-10 23:59:40 -0600

    Delete Python-2-cifrado directory

--- Python-2-cifrado/Autenticación HMAC-15.py
+++
@@ -1,8 +0,0 @@
-import hmac
-import hashlib
-
-def autenticar_hmac(clave, mensaje):
-    return hmac.new(clave.encode(), mensaje.encode(), hashlib.sha256).hexdigest()
-
-# Ejemplo
-print(autenticar_hmac("clave_secreta", "Hola Mundo"))
